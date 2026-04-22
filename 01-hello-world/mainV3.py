a0bdf8f3f0dec50db76a9137bb7ed239d51c350f
Yuri <yc.pernambuco@gmail.com>
2026-04-22 16:19:56 -0300

    Olá github! Com inputs do usuário v3

--- 01-hello-world/mainV3.py
+++ 01-hello-world/mainV3.py
@@ -3,15 +3,12 @@ print("Olá amigo(a)! 🚀")
 nome = input("Qual é o seu nome?  ")
 idade = int(input("Qual é a sua idade?  "))
 
-if idade < 12:
-        print("Você é uma criança!")
-elif idade < 18:
-        print("Você é menor de idade!")
-else:
-        print("Você é maior de idade!")
-
 if idade < 18:
         falta = 18 - idade
+        if idade < 12:
+                print("Você é uma criança!")
+        else:
+                print("Você é menor de idade!")
         print(f"Faltam {falta} anos para você se tornar maior de idade. Seja muito bem-vindo(a) ao meu repositório do GitHub! 🌟")
-else :
-        print("Seja muito bem-vindo(a) ao meu repositório do GitHub! 🌟")
+else:
+        print("Você é maior de idade!", "Seja muito bem-vindo(a) ao meu repositório do GitHub! 🌟")
